9c9abb5c1eeb478c4f0bd202bbdaeaa4a130b7f2
mizuha <mizuha.watanabe@gmail.com>
2015-06-22 19:34:44 +0900

    first commit

---
+++ diffusion/diffusion.cpp
@@ -0,0 +1,104 @@
+// diffusion.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define N	  10000		// 時間ステップ数
+#define X	  100		// メッシュ数
+#define time  1.0		// 時間の計算範囲
+#define space 1.0		// 空間の計算範囲
+#define D	  0.4		// 拡散係数
+#define write 100		// 何ステップに一回書きこむか
+
+double u[N][X] = {};
+double du[N][X] = {};
+double x[X] = {};
+double S[N][X] = {};
+double dt, dx;
+double a[4] = { 0, 0.5, 0.5 ,1 };
+double b[4] = { (double)1 / 6, (double)1 / 3, (double)1 / 3, (double)1 / 6 };
+double valid;
+int    i, n, k, m;
+FILE   *fp;
+char   fname[32];
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// 数値安定性の計算、不安定ならエラー
+	dt = time / N;
+	dx = space / X;
+	valid = D * dt / (dx * dx);
+	if (valid > 0.5){
+		printf("ERROR : Calculation is unstable.(%e)\n", valid);
+		getchar();
+		return -1;
+	}
+	else{
+		printf("Calculation is stable.(%e)\n", valid);
+	}
+
+	// アウトプットフォルダの初期化
+	system("rd /s /q output");
+	system("mkdir output");
+
+	// 初期条件
+	for (i = 0; i < X; i++){
+		u[0][i] = 0;
+	}
+
+	// Runge-Kutta法の計算
+	for (n = 0; n < N; n++){
+
+		//空間の初期化
+		for (i = 0; i < X; i++){
+			x[i] = i * dx;
+		}
+
+		// アウトプットファイルの書き出し
+		if (n % write == 0){
+			sprintf(fname, "output/output_%d.csv", m);
+			fp = fopen(fname, "w");
+			if (fp == NULL){
+				printf("cannot open output file.");
+				return -1;
+			}
+			fprintf(fp, "x,u\n");
+			for (i = 0; i < X; i++){
+				fprintf(fp, "%e,%e\n", x[i], u[n][i]);
+			}
+			fclose(fp);
+			m += 1;
+		}
+		
+		// 第一種境界条件
+		u[n][0] = 1;
+
+		// 4次近似の計算
+		for (k = 0; k < 4; k++){
+			for (i = 1; i < X - 1; i++){
+				u[n][i] += a[k] * du[n][i];
+				x[i] += a[k] * dx;
+				du[n][i] = dt * D * ((u[n][i + 1] + u[n][i - 1] - 2 * u[n][i]) / (dx * dx) + (u[n][i + 1] - u[n][i - 1]) / (x[i] * dx));
+				S[n][i] += b[k] * du[n][i];
+			}
+		}
+
+		// 第二種境界条件
+		u[n][X - 1] = u[n][X - 2];
+		
+		// 時間発展
+		for (i = 0; i < X; i++){
+			u[n + 1][i] = u[n][i] + S[n][i];
+		}
+	}
+
+	// 計算終了の確認
+	printf("Calculation is complete. Please push enter key.");
+	getchar();
+
+	return 0;
+}
+
